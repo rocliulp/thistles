3c9880cd5963713e05a90effaea1686d04fe57a1
Paul <rocliulp@yahoo.com.cn>
2013-06-13 18:06:04 +0800

    Changed a little on the code.

--- src/ErrMsg.h
+++ src/ErrMsg.h
@@ -5,11 +5,14 @@
 #ifndef _ERR_MSG_H_
 #define _ERR_MSG_H_
 
-#define FAILED_CREATING_LOGGER "Failed creating logger. Please check if there is memory available."
-#define FAILED_CLEARING_LOGGER "Failed clearing logger. Please check if there is memory leak."
-#define IMPOSSIBLE_CASE "Impossible case happen. Please check your logic."
-#define FAILED_CREATING_STRING "Failed creating string. Please check if there is memory available."
-#define FAILED_CLEARING_STRING "Failed clearing string. Please check if there is memory leak."
-#define FAILED_FORKING "Failed forking."
+/*
+ * @TODO: methods / macros to format string by appending \n trailing.
+ */
+#define FAILED_CREATING_LOGGER L"Failed creating logger. Please check if there is memory available."
+#define FAILED_CLEARING_LOGGER L"Failed clearing logger. Please check if there is memory leak."
+#define IMPOSSIBLE_CASE L"Impossible case happen. Please check your logic."
+#define FAILED_CREATING_STRING L"Failed creating string. Please check if there is memory available."
+#define FAILED_CLEARING_STRING L"Failed clearing string. Please check if there is memory leak."
+#define FAILED_FORKING L"Failed forking."
 
 #endif
